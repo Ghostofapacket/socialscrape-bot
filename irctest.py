9fd70d5b01a9615e18da3c1963c83019057ef878
Matt Iggo <matt@iggo.co.uk>
2019-06-10 20:35:04 +0100

    Update irctest.py

--- irctest.py
+++ irctest.py
@@ -6,12 +6,12 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 HOST = 'irc.netgamers.org' #irc server
 PORT = 6667 #port
 NICK = 'testbot1234'
-USERNAME = 'testbot1234'
+USERNAME = 'Igloo'
 REALNAME = 'little pony'
 
 print('soc created |', s)
 remote_ip = socket.gethostbyname(HOST)
-print('ip of irc server is:', remote_ip)
+#print('ip of irc server is:', remote_ip)
 
 
 s.connect((HOST, PORT))
@@ -22,7 +22,7 @@ nick_cr = ('NICK ' + NICK + '\r\n').encode()
 s.send(nick_cr)
 usernam_cr= ('USER megadeath megadeath megadeath :rainbow pie \r\n').encode()
 s.send(usernam_cr)
-s.send('JOIN #ibot \r\n'.encode()) #chanel
+s.send('JOIN ibot \r\n'.encode()) #chanel
 
 while 1:
     data = s.recv(4096).decode('utf-8')
