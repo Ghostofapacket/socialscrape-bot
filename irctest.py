95c491706bc93ab91b959073a0d0036a7985180a
Matt Iggo <matt@iggo.co.uk>
2019-06-10 20:58:14 +0100

    Update irctest.py

--- irctest.py
+++ irctest.py
@@ -24,9 +24,9 @@ nick_cr = ('NICK ' + NICK + '\r\n').encode()
 s.send(nick_cr)
 usernam_cr= ('USER megadeath megadeath megadeath :rainbow pie \r\n').encode()
 s.send(usernam_cr)
-#s.send('JOIN ##ibot \r\n'.encode()) #chanel
+s.send('JOIN #ibot \r\n'.encode()) #chanel
 time.sleep(1)
-s.send(bytes("JOIN ##ibot \n", "UTF-8"))
+#s.send(bytes("JOIN ##ibot \n", "UTF-8"))
 
 while 1:
     data = s.recv(4096).decode('utf-8')
